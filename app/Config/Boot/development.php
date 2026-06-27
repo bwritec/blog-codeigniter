d6223f8868c97e78c5aaa3335246a2264aaae771
Jorge Luiz <bjxjorge@gmail.com>
2026-06-27 20:33:30 -0300

    .

--- app/Config/Boot/development.php
+++ app/Config/Boot/development.php
@@ -1,34 +1,37 @@
 <?php
 
-/*
- |--------------------------------------------------------------------------
- | ERROR DISPLAY
- |--------------------------------------------------------------------------
- | In development, we want to show as many errors as possible to help
- | make sure they don't make it to production. And save us hours of
- | painful debugging.
- |
- | If you set 'display_errors' to '1', CI4's detailed error report will show.
- */
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+    /**
+     *--------------------------------------------------------------------------
+     * ERROR DISPLAY
+     *--------------------------------------------------------------------------
+     * Durante o desenvolvimento, queremos exibir o maior número
+     * possível de erros para garantir que eles não cheguem à
+     * produção — e para nos poupar de horas de depuração penosa.
+     *
+     * Se você definir 'display_errors' como '1', o relatório
+     * detalhado de erros do CI4 será exibido.
+     */
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
 
-/*
- |--------------------------------------------------------------------------
- | DEBUG BACKTRACES
- |--------------------------------------------------------------------------
- | If true, this constant will tell the error screens to display debug
- | backtraces along with the other error information. If you would
- | prefer to not see this, set this value to false.
- */
-defined('SHOW_DEBUG_BACKTRACE') || define('SHOW_DEBUG_BACKTRACE', true);
+    /**
+     *--------------------------------------------------------------------------
+     * DEBUG BACKTRACES
+     *--------------------------------------------------------------------------
+     * Se definida como true, essa constante fará com que as telas
+     * de erro exibam rastreamentos de depuração (*debug backtraces*)
+     * juntamente com as demais informações de erro. Caso prefira não
+     * visualizar isso, defina esse valor como false.
+     */
+    defined('SHOW_DEBUG_BACKTRACE') || define('SHOW_DEBUG_BACKTRACE', true);
 
-/*
- |--------------------------------------------------------------------------
- | DEBUG MODE
- |--------------------------------------------------------------------------
- | Debug mode is an experimental flag that can allow changes throughout
- | the system. This will control whether Kint is loaded, and a few other
- | items. It can always be used within your own application too.
- */
-defined('CI_DEBUG') || define('CI_DEBUG', true);
+    /**
+     *--------------------------------------------------------------------------
+     * DEBUG MODE
+     *--------------------------------------------------------------------------
+     * O modo de depuração é uma flag experimental que pode permitir
+     * alterações em todo o sistema. Ele controla se o Kint é carregado,
+     * além de alguns outros itens. Também pode ser utilizado na sua
+     * própria aplicação.
+     */
+    defined('CI_DEBUG') || define('CI_DEBUG', true);
